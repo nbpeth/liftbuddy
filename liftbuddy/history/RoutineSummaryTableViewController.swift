d30654e7f39cb2d3275980c1919a550622bbef43
Nick Peth <nickpeth@carfax.com>
2017-12-25 14:29:49 -0600

    routine lift details controller added and displayed on history page

--- liftbuddy/history/RoutineSummaryTableViewController.swift
+++ liftbuddy/history/RoutineSummaryTableViewController.swift
@@ -6,7 +6,34 @@ class RoutineSummaryTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(completedRoutine)
-        print(historicalRoutine)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "routineSummaryTableViewCell") as? RoutineSummaryTableViewCell
+            else { return RoutineSummaryTableViewCell() }
+        let liftRelativeToWorkout = Int(ceil(Double(indexPath.row) / (Double(indexPath.section + 1))))
+    
+        cell.workoutNameLabel.text = completedRoutine?.workout[indexPath.section].name
+        cell.weightLabel.text = "Weight: \(String(describing:completedRoutine?.workout[indexPath.section].lifts[liftRelativeToWorkout].weight.value ?? 0))"
+        cell.repsLabel.text = "Reps: \(String(describing:completedRoutine?.workout[indexPath.section].lifts[liftRelativeToWorkout].reps.value ?? 0))"
+        cell.setNumberLabel.text = "\(String(describing:liftRelativeToWorkout + 1)). "
+        
+        if indexPath.section % 2 == 0 {
+            cell.backgroundColor = Theme.cellAlternateBackgroundColor
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let completedRoutine = completedRoutine else { return 0 }
+        
+        return completedRoutine.workout[section].lifts.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        guard let completedRoutine = completedRoutine else { return 0 }
+        
+        return completedRoutine.workout.count
     }
 }
---
+++ liftbuddy/history/table cells/RoutineSummaryTableViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class RoutineSummaryTableViewCell: UITableViewCell {
+    @IBOutlet weak var workoutNameLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var repsLabel: UILabel!
+    @IBOutlet weak var setNumberLabel: UILabel!
+    
+}
